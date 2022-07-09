f686a78f48e798d6c6da324f15ecd86814411e2b
asdfzlcl <1105374599@qq.com>
2022-07-09 16:08:12 +0800

    后端拆迁完成

--- src/application/app/FuncInjectorImpl.java
+++ src/application/app/FuncInjectorImpl.java
@@ -39,56 +39,5 @@ public class FuncInjectorImpl implements FuncInjector {
         return filedictionary;
     }
 
-    //获取文件精度、纬度、高度上下限
-    public String Getlimit(JSObject params){
-        InputParam inputParam = new InputParam(params);
-        System.out.println(inputParam);
-        List<String> data = new ArrayList<>(200);
-        return data.toString();
-    }
 
-    public String Getinformation(JSObject params){
-        InputParam inputParam = new InputParam(params);
-        //System.out.println(inputParam);
-        List<String> data = new ArrayList<>();
-        data.add("-90");
-        data.add("90");
-        data.add("-180");
-        data.add("180");
-//        data.add(Latitudes.get(0).toString());
-//        data.add(Latitudes.get(Latitudes.size()-1).toString());
-//        data.add(Longitudes.get(0).toString());
-//        data.add(Longitudes.get(Longitudes.size()-1).toString());
-        data.add(heights.get(0).toString());
-        data.add(heights.get(heights.size()-1).toString());
-        return data.toString();
-    }
-
-
-    public String GetFileHeight(JSObject params){
-        InputParam inputParam = new InputParam(params);
-        System.out.println(inputParam);
-        List<String> data = new ArrayList<>();
-        return data.toString();
-    }
-
-    public void StartExe(String adress)
-    {
-        Runtime rt = Runtime.getRuntime();
-        try {
-            rt.exec(adress);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public String GetFileLat()
-    {
-        return Latitudes.toString();
-    }
-
-    public String GetFileLon()
-    {
-        return Longitudes.toString();
-    }
 }
