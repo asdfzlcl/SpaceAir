a99769ef9b3ec6ee427f144037a4e55ff7c6e207
FranzChu <70999398+FranzChu@users.noreply.github.com>
2023-04-03 19:42:29 +0800

    feat: 视图层重构第一次提交

--- src/application/pages/js/index.databind.js
+++
@@ -1,8 +0,0 @@
-let bindGetter = function(obj,attr,domSelector,
-                          getter=()=>document.querySelector(domSelector).value){
-    Object.defineProperty(obj, attr, {
-        get: getter
-    });
-}
-
-bindGetter(params,'type',"input[name=type-selector]:checked")
